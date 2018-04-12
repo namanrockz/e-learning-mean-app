920521ca9a823f100d300e754531a5b2e8050542
namanrockz <chauhan.naman12@gmail.com>
2018-04-12 15:41:43 +0530

    Intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  img5 = 'images/5.jpg';
+  img4 = 'images/4.jpg';
+  img6 = 'images/g6.jpg';
+
+  users: Array<any>;
+
+  constructor(private _dataService: DataService) {
+
+    this._dataService.getUsers()
+      .subscribe(res => this.users = res);
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import { DataService } from './data.service';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
+const rt: Routes = [
+  {path:'signup',component: SignupComponent },
+  {path:'signin',component: SigninComponent },
+  {path:'home',component: HomeComponent },
+  {path:'courses',component: CoursesComponent },
+  {path:'',redirectTo: '/home',pathMatch:'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CoursesComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(rt)
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
